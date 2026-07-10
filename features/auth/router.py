71387dbaf0b63ce5d85763aa128fac79765f836b
yihan <yu66946@yusco.com.tw>
2026-07-10 17:12:21 +0800

    增加line auth相關的api

--- features/auth/router.py
+++ features/auth/router.py
@@ -15,3 +15,10 @@ def user_login_by_line():
     scope = ''
     nonce = ''
     print(f'https://access.line.me/oauth2/v2.1/authorize?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&state={state}&scope={scope}&nonce={nonce}')
+
+
+
+# 接收flutter回傳的line auth code
+@router.post('/line/login/')
+def auth_line_login():
+    pass
